299f8e91e49c6603077f937e667a86ddbd053548
Connor Boesch <Connor_Boesch@yahoo.com>
2016-04-18 14:36:54 -0500

    abstract product class

---
+++ FactoryStructural/product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The abstract "product" class.
+/// </summary>
+namespace FactoryStructural {
+	abstract class product {
+	}
+}
