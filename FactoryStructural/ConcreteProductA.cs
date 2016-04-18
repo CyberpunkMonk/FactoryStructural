78ac1dc2b9712f606421f5e345b6a5bba175a35f
Connor Boesch <Connor_Boesch@yahoo.com>
2016-04-18 15:01:40 -0500

    ConcreteProductA class

---
+++ FactoryStructural/ConcreteProductA.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FactoryStructural {
+	/// <summary>
+	/// The "ConcreteProductA" class
+	/// </summary>
+	/// <seealso cref="FactoryStructural.product" />
+	class ConcreteProductA:product{
+	}
+}
